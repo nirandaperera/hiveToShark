c3a674c653334df913ad19b865b341974fb815a9
niranda perera <niranda@wso2.com>
2014-08-14 14:48:33 +0530

    small changes to a class

--- sharkClient/src/main/java/sharkClient_AM.java
+++ sharkClient/src/main/java/sharkClient_AM.java
@@ -23,7 +23,9 @@ public class sharkClient_AM {
 
         String req_tbl_attrib = "key STRING,api STRING,api_version STRING,consumerKey STRING,userId STRING,context STRING,version STRING, request INT, requestTime BIGINT, resourcePath STRING, method STRING, hostName STRING, apiPublisher STRING";
         String cass_sto_hand = "'org.apache.hadoop.hive.cassandra.CassandraStorageHandler'";
-        String cass_serde_props = cass_ds_props + ", \"cassandra.cf.name\" = \"org_wso2_apimgt_statistics_request\",\"cassandra.columns.mapping\" = \":key,payload_api, payload_api_version,payload_consumerKey,payload_userId,payload_context,payload_version, payload_request, payload_requestTime, payload_resourcePath, payload_method,payload_hostName,payload_apiPublisher\"";
+        String cass_serde_props = cass_ds_props +
+                ", \"cassandra.cf.name\" = \"org_wso2_apimgt_statistics_request\"," +
+                "\"cassandra.columns.mapping\" = \":key,payload_api, payload_api_version,payload_consumerKey,payload_userId,payload_context,payload_version, payload_request, payload_requestTime, payload_resourcePath, payload_method,payload_hostName,payload_apiPublisher\"";
 //        String cass_ds_config = "\"cassandra.host\"=\"localhost\",\"cassandra.port\"=\"9163\",\"cassandra.ks.name\"=\"EVENT_KS\",\"cassandra.ks.username\"=\"admin\",\"cassandra.ks.password\"=\"admin\"";
 //        String cass_cf_map = "\"cassandra.cf.name\" = \"org_wso2_apimgt_statistics_request55\",\"cassandra.columns.mapping\" = \":key,payload_api, payload_api_version,payload_consumerKey,payload_userId,payload_context,payload_version, payload_request, payload_requestTime, payload_resourcePath, payload_method,payload_hostName,payload_apiPublisher\"";
 
@@ -36,7 +38,10 @@ public class sharkClient_AM {
 
         String req_sum_tbl_attrib = "api STRING, api_version STRING, version STRING, apiPublisher STRING, consumerKey STRING,userId STRING,context STRING, max_request_time BIGINT, total_request_count INT, hostName STRING,year SMALLINT,month SMALLINT,day SMALLINT, time STRING";
         String jdbc_sto_hand = "'org.wso2.carbon.hadoop.hive.jdbc.storage.JDBCStorageHandler'";
-        String jdbc_tbl_props = jdbc_ds_props + ", 'hive.jdbc.update.on.duplicate' = 'true', 'hive.jdbc.primary.key.fields'='api,api_version,version,apiPublisher,consumerKey,userId,context,hostName,time',\t'hive.jdbc.table.create.query' = 'CREATE TABLE API_REQUEST_SUMMARY ( api VARCHAR(100), api_version VARCHAR(100), version VARCHAR(100), apiPublisher VARCHAR(100),consumerKey VARCHAR(100),userId VARCHAR(100), context VARCHAR(100), max_request_time BIGINT, total_request_count INT, hostName VARCHAR(100), year SMALLINT, month SMALLINT, day SMALLINT, time VARCHAR(30),PRIMARY KEY(api,api_version,apiPublisher,consumerKey,userId,context,hostName,time))'";
+        String jdbc_tbl_props = jdbc_ds_props +
+                ", 'hive.jdbc.update.on.duplicate' = 'true', " +
+                "'hive.jdbc.primary.key.fields'='api,api_version,version,apiPublisher,consumerKey,userId,context,hostName,time', " +
+                "'hive.jdbc.table.create.query' = 'CREATE TABLE API_REQUEST_SUMMARY ( api VARCHAR(100), api_version VARCHAR(100), version VARCHAR(100), apiPublisher VARCHAR(100),consumerKey VARCHAR(100),userId VARCHAR(100), context VARCHAR(100), max_request_time BIGINT, total_request_count INT, hostName VARCHAR(100), year SMALLINT, month SMALLINT, day SMALLINT, time VARCHAR(30),PRIMARY KEY(api,api_version,apiPublisher,consumerKey,userId,context,hostName,time))'";
 
         query = "CREATE EXTERNAL TABLE IF NOT EXISTS APIRequestSummaryData (" + req_sum_tbl_attrib + ") STORED BY " + jdbc_sto_hand + " TBLPROPERTIES (" + jdbc_tbl_props + ")";
         System.out.println(query);
@@ -45,12 +50,28 @@ public class sharkClient_AM {
         long endTime = System.nanoTime();
         System.out.println(result.toString() + " Elapsed time: " + (endTime - startTime));
 
-        query = "insert overwrite table APIRequestSummaryData select api, api_version,version, apiPublisher, COALESCE(consumerKey,''),userId,context,max(requestTime) as max_request_time,sum(request) as total_request_count,hostName,  year(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as year, month(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as month,day(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as day,concat(substring(from_unixtime(cast(requestTime/1000 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),0,16),':00') as time from APIRequestData group by api,api_version,version,apiPublisher,consumerKey,userId,context,hostName,year(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )), month(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )),day(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )),hour(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )),minute(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )),substring(from_unixtime(cast(requestTime/1000 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),0,16)";
+        query = "insert overwrite table APIRequestSummaryData " +
+                "select api, api_version,version, apiPublisher, COALESCE(consumerKey,''),userId,context," +
+                "max(requestTime) as max_request_time, sum(request) as total_request_count, hostName,  " +
+                "year(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as year, " +
+                "month(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as month," +
+                "day(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )) as day," +
+                "concat(substring(from_unixtime(cast(requestTime/1000 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),0,16),':00') as time " +
+                "from APIRequestData " +
+                "group by " +
+                "api,api_version,version,apiPublisher,consumerKey,userId,context,hostName," +
+                "year(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' )), " +
+                "month(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' ))," +
+                "day(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' ))," +
+                "hour(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' ))," +
+                "minute(from_unixtime(cast(requestTime/1000 as BIGINT),'yyyy-MM-dd HH:mm:ss.SSS' ))," +
+                "substring(from_unixtime(cast(requestTime/1000 as BIGINT), 'yyyy-MM-dd HH:mm:ss'),0,16)";
         System.out.println(query);
         result = stmt.executeQuery(query);
         System.out.println(result.toString());
 
         con.close();
+
         System.out.println("End");
     }
 }
